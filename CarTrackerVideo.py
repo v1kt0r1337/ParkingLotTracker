71ac2679fe63c77685b5decd7b79fcfe72af6695
AdamAjmi <adam.ajmi@gmail.com>
2017-05-18 13:06:37 +0200

    Added morphological transformation to clean up foreground mask on video playback

--- CarTrackerVideo.py
+++ CarTrackerVideo.py
@@ -4,6 +4,8 @@ import numpy as np
 car_hc = cv2.CascadeClassifier('cars.xml')
 vid = cv2.VideoCapture('video1.avi')
 bgs = cv2.createBackgroundSubtractorMOG2(detectShadows = True)
+opMorph = np.ones((4,4),np.uint8)
+clMorph = np.ones((4,4),np.uint8)
 
 if vid.isOpened():
     rval , frame = vid.read()
@@ -13,6 +15,12 @@ while rval:
     rval, frame = vid.read()
 	
 	fgmask = bgs.apply(frame)
+	
+	rval, imBin=cv2.threshold(fgmask, 200, 255, cv2.TRESH_BINARY)
+	
+	fgmask = cv.morphologyEx(imBin, cv2.MORPH_OPEN, opMorph)
+	
+	fgmask = cv2.morphologyEx(imBin, cv2.MORPH_CLOSE, clMorph)
 	
     cars = car_hc.detectMultiScale(frame, 1.1, 2)
 
